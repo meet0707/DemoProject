6b8ce21716652dc17c8797761e35fc3a560d095b
Meet Thakkar <thakkarmeet1357@gmail.com>
2024-02-10 13:38:52 +0000

    Initial Commit

---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Meet Thakkar on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
